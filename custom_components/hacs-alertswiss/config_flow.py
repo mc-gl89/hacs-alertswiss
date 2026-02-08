2542b09c8791985e859f8ddcbe397c3723b1e7c2
Gee <mc-gl@hotmail.com>
2026-02-08 15:07:10 +0100

    bugfix Config and add Icon

--- custom_components/hacs-alertswiss/config_flow.py
+++ custom_components/hacs-alertswiss/config_flow.py
@@ -3,6 +3,7 @@ from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.helpers import config_validation as cv
 from .const import DOMAIN, CONF_RADIUS, DEFAULT_RADIUS
+
 class AlertSwissConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
